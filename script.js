1c767fd97835eb831a16352bdb9ded47ba70c609
Centilus Buchanan <36555074+Centilus1272@users.noreply.github.com>
2023-09-19 12:49:55 -0500

    Creation of Main Files

---
+++ script.js
@@ -0,0 +1,11 @@
+// Add JavaScript functionality here if needed
+document.getElementById("donateButton").addEventListener("click", function() {
+    // Add code to handle donation processing or redirect to a donation page
+    // You may use external donation platforms like Stripe or PayPal for processing donations.
+    // Example: window.location.href = "https://www.example.com/donate";
+});
+
+document.getElementById("volunteerButton").addEventListener("click", function() {
+    // Add code to handle volunteer sign-up or redirect to a volunteer page
+    // Example: window.location.href = "https://www.example.com/volunteer";
+});
